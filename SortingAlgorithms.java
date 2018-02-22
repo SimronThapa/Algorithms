ec08da2be33de6bbe957fd6f7257a0e8798610b5
Simron Thapa <35314994+SimronThapa@users.noreply.github.com>
2018-02-21 23:26:59 -0600

    Update SortingAlgorithms.java
    Worst case and best case senario added.

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -13,6 +13,8 @@ public class SortingAlgorithms {
      */
     public static void main(String[] args) {
         int[] inputList = {23,56,12,9,34,29,14,2,87,45,34,1};
+        //int[] inputList = {9,8,7,6,5,4,3,2,1} <---worst case senario
+        //int[] inputList = {1,2,3,4,5,6,7,8,9} <---best case senario
         System.out.println(Arrays.toString(inputList));
         
         System.out.println("Enter 1 for Bubble Sort, 2 for Selection Sort, 3 for Insertion Sort:");
